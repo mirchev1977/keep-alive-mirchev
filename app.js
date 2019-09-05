d73945ebe6967094e945f8bcab016773f56b5a0c
mirchev1977 <mirchev.notes@gmail.com>
2019-09-05 15:49:10 +0300

    again keep alive

--- app.js
+++ app.js
@@ -3,40 +3,49 @@ const https   = require( 'https'   );
 
 const app = express();
 
-//keepAlive();
-
-//function keepAlive () {
-//    clearInterval();
-//    setInterval( () => {
-//        //"https://nodejs-proj-002-our-library.herokuapp.com"
-//        try {
-//            https.get( 
-//            'https://nodejs-proj-002-our-library.herokuapp.com', 
-//                res => {
-//                 console.log( 'nodejs-proj-002' );
-//             });
-//        } catch ( err ) {
-//            console.log( err );
-//        };
-//
-//        try {
-//            https.get(
-//                "https://nodejs-proj-004-library-mvc.herokuapp.com/admin/restore",
-//                resp => {
-//                 console.log( 'nodejs-proj-004' );
-//                });
-//        } catch ( err ) {
-//            console.log( err );
-//        };
-//
-//        keepAlive();
-//    }, 10 * 1000 );
-//}
 
 app.use( '/', ( req, res, next ) => {
+    keepAlive();
     res.write( 'It is working' );
     res.end();
 }  );
+
+function keepAlive () {
+    clearInterval();
+    setInterval( () => {
+        //"https://nodejs-proj-002-our-library.herokuapp.com"
+        try {
+            https.get( 
+            'https://keep-alive-mirchev.herokuapp.com', 
+                res => {
+                 console.log( 'proj-keep-alive' );
+             });
+        } catch ( err ) {
+            console.log( err );
+        };
+
+        try {
+            https.get( 
+            'https://nodejs-proj-002-our-library.herokuapp.com', 
+                res => {
+                 console.log( 'nodejs-proj-002' );
+             });
+        } catch ( err ) {
+            console.log( err );
+        };
+
+        try {
+            https.get(
+                "https://nodejs-proj-004-library-mvc.herokuapp.com/admin/restore",
+                resp => {
+                 console.log( 'nodejs-proj-004' );
+                });
+        } catch ( err ) {
+            console.log( err );
+        };
+
+    }, 10 * 1000 );
+}
 
 
 app.listen( process.env.PORT || 5000 );
