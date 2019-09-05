9d3272b9d45e027405bbaedbd59ead458111cc01
mirchev1977 <mirchev.notes@gmail.com>
2019-09-05 15:40:27 +0300

    created a route

--- app.js
+++ app.js
@@ -5,29 +5,38 @@ const app = express();
 
 keepAlive();
 
-function keepAlive () {
-    setInterval( () => {
-        //"https://nodejs-proj-002-our-library.herokuapp.com"
-        try {
-            https.get( 
-            'https://nodejs-proj-002-our-library.herokuapp.com', 
-                res => {
-                 //console.log( 'nodejs-proj-002-our-library.herokuapp.com' );
-             });
-        } catch ( err ) {
-            console.log( err );
-        };
+//function keepAlive () {
+//    clearInterval();
+//    setInterval( () => {
+//        //"https://nodejs-proj-002-our-library.herokuapp.com"
+//        try {
+//            https.get( 
+//            'https://nodejs-proj-002-our-library.herokuapp.com', 
+//                res => {
+//                 console.log( 'nodejs-proj-002' );
+//             });
+//        } catch ( err ) {
+//            console.log( err );
+//        };
+//
+//        try {
+//            https.get(
+//                "https://nodejs-proj-004-library-mvc.herokuapp.com/admin/restore",
+//                resp => {
+//                 console.log( 'nodejs-proj-004' );
+//                });
+//        } catch ( err ) {
+//            console.log( err );
+//        };
+//
+//        keepAlive();
+//    }, 10 * 1000 );
+//}
 
-        try {
-            https.get(
-                "https://nodejs-proj-004-library-mvc.herokuapp.com/admin/restore",
-                resp => {
-                 //console.log( 'nodejs-proj-004-library-mvc.herokuapp.com/admin/restore' );
-                });
-        } catch ( err ) {
-            console.log( err );
-        };
-    }, 15 * 60 * 1000 );
-}
+app.use( ( req, res, next ) => {
+    res.write( 'It is working' );
+    res.end();
+}  );
+
 
 app.listen( process.env.PORT || 5000 );
